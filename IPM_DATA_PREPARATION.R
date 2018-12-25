f14181cc2f49415a72f131d952384941f8d7be12
steffenoppel <steffen.oppel@gmail.com>
2018-12-25 19:38:25 +0000

    summarised IPM input and abandoned hope for multi-state input as records are too sparse

--- IPM_DATA_PREPARATION.R
+++ IPM_DATA_PREPARATION.R
@@ -16,7 +16,7 @@ library(data.table)
 ##   1. SPECIFY THE SPECIES AND START YEAR FOR WHICH YOU WANT A SUMMARY ####
 #############################################################################
 SP<-"AYNA"
-start<-2000
+start<-1981
 
 
 
@@ -31,8 +31,8 @@ try(setwd("C:\\STEFFEN\\RSPB\\UKOT\\Gough\\DATA\\Breeding_Database"), silent=T)
 load("GOUGH_seabird_data.RData")
 
 ## run the RODBC import of CMR data in a 32-bit version of R
-system(paste0(Sys.getenv("R_HOME"), "/bin/i386/Rscript.exe ", shQuote("C:\\STEFFEN\\RSPB\\UKOT\\Gough\\ANALYSIS\\RODBC_CMR_import.R")), wait = TRUE, invisible = FALSE)
-try(setwd("C:\\STEFFEN\\RSPB\\UKOT\\Gough\\ANALYSIS"), silent=T)
+system(paste0(Sys.getenv("R_HOME"), "/bin/i386/Rscript.exe ", shQuote("C:\\STEFFEN\\RSPB\\UKOT\\Gough\\ANALYSIS\\SeabirdSurvival\\RODBC_CMR_import.R")), wait = TRUE, invisible = FALSE)
+try(setwd("C:\\STEFFEN\\RSPB\\UKOT\\Gough\\ANALYSIS\\SeabirdSurvival"), silent=T)
 load("GOUGH_seabird_CMR_data.RData")
 
 ## import summary file from Alex Bond (origin of data cannot be verified!!)
@@ -46,7 +46,6 @@ counts<-counts %>% filter(Species==SP)
 
 
 ## look at data
-head(contacts)  ## CMR data
 head(nests)  ## nest monitoring data
 head(counts)  ## seabird count data
 head(backup)
@@ -87,8 +86,7 @@ FECUND
 
 ### PLOT TO SPOT ANY OUTLIERS OF BREEDING SUCCESS
 ggplot(FECUND, aes(x=Year,y=BREED_SUCC)) +geom_point(size=2, color='darkred')+geom_smooth(method='lm') 
-
-
+fwrite(FECUND,"AYNA_breed_success_1982_2017.csv")
 
 
 
@@ -111,17 +109,80 @@ POPSIZE<-counts %>% filter(Species==SP) %>%
   summarise(N=sum(Number, na.rm=T)) %>%
   spread(key=Colony, value=N)
 POPSIZE[19:35,]
-
-
-### add missing years from backup data (unknown source)
-POP2<-backup %>% mutate(Colony="Area 1") %>%
-  select(Year,Colony,INCU) %>%
-  #filter(!Year %in% FECUND$Year) %>%       ### remove the years already covered by good data
-  spread(key=Colony, value=INCU)
+fwrite(POPSIZE,"AYNA_pop_counts_1982_2018.csv")
 
 
 
 
 
 
+#############################################################################
+##   5. PREPARE THE MARK-RECAPTURE DATA FOR SURVIVAL ANALYSIS ###############
+#############################################################################
+
+### checked on 25 Dec 2018: CMR database does not have satisfactory level of age or breeding status assignment for vast majority of contacts
+## including 'age' and 'breeding status' reduces number of contacts from ~49000 to ~6000
+## including the side on which a federal band was applied reduces the AYNA contacts from 25201 to 7786
+## the only AYNA ringed as 'Chick' are from the 2015-16 season
+## given the gross inadequacies of past records it is safer to simply use 0/1 contacts and assume all birds are breeding
+## created new query called 'metalside' which extracts the recorded side of the body for the metal ring - all birds ringed as chicks should have "L"
+
+
+head(contacts)  ## CMR data
+dim(contacts)
+
+### REMOVE RECORDS FROM BEFORE THE SET START YEAR
+contacts<-contacts %>%
+  filter(year(Date_Time)>start) 
+dim(contacts)
+
+
+### FIND MISSING DATA FOR SEASON AND REPLACE BASED ON DATE
+contacts<-contacts %>%
+  mutate(Contact_Season=as.character(Contact_Season)) %>%
+  mutate(MO=month(Date_Time)) %>%
+  mutate(Season1=paste(Contact_Year,(as.numeric(substr(Contact_Year,3,4))+1),sep="-")) %>%
+  mutate(Season2=paste(Contact_Year-1,substr(Contact_Year,3,4),sep="-")) %>%
+  mutate(Contact_Season=if_else(is.na(Contact_Season), if_else(MO>6,Season1,Season2),Contact_Season)) %>%
+  select(BirdID,Location,Contact_Season)
+dim(contacts)
+
+
+
+# ### DETERMINE STATE FOR MULTI-STATE MODEL - ABANDONED on 25 Dec 2018 because records were too incomplete
+# ## State 1 = chick
+# ## State 2 = non-breeder
+# ## State 3 = breeder
+# stateContacts<-contacts %>%
+#   mutate(Age=as.character(Age)) %>%
+#   mutate(Age=if_else(is.na(Age),"Adult",Age)) %>%       ### BASED ON ASSUMPTION FROM QUICK CHECK IN 2018 - only adults had age lacking
+#   mutate(STATE=if_else(Age=="Chick",1,2)) %>%
+#   mutate(STATE=if_else(STATE>1 & Breeding_Status %in% c("Unknown","Loafing","Holding","Building","Bird on empty nest"),2,3))
+# head(stateContacts)
+
+
+### CREATE SIMPLE ENCOUNTER HISTORY (0/1)
+AYNA_EH<-contacts %>% select(BirdID,Contact_Season) %>%
+  mutate(count=1) %>%
+  group_by(BirdID,Contact_Season) %>%
+  summarise(STATE=max(count)) %>%
+  spread(key=Contact_Season, value=STATE, fill=0)
+AYNA_EH
+
+
+### TRY TO ASSIGN AGE AT FIRST MARK FROM SIDE OF BODY
+head(metalside)
+birdage<-metalside %>% mutate(minage=if_else(Side_Of_Body=="L",0,1)) %>%
+  group_by(SpeciesCode,BirdID) %>%
+  summarise(MinAge=min(minage)) %>%
+  filter(SpeciesCode==SP)
+dim(birdage)
+
+
+### INSERT AGE INTO ENCOUNTER HISTORY
+AYNA_EH$AGE<-birdage$MinAge[match(AYNA_EH$BirdID,birdage$BirdID)]
+
+### EXPORT ENCOUNTER HISTORY
+dim(AYNA_EH)
+fwrite(AYNA_EH[,c(1,39,2:38)],"AYNA_multistate_encounter_history_1982_2018.csv")
 
